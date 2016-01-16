8b68e1e94b9a8cebc17da975c79d6b65f7378e9e
julien_de_loor <julien.deloor@gmail.com>
2016-01-16 18:07:56 -0500

    Just save from work this useless started file

---
+++ __init__.py
@@ -0,0 +1,14 @@
+bl_info = {
+    "name": "Ogre Blender Importer",
+    "category": "Import-Export",
+}
+
+import bpy
+
+def register():
+
+def unregister():
+
+
+if __name__ == "__main__":
+    register()
